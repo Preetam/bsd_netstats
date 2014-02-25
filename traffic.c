d692cd56432b809c6a1caa473cb499d58e7c9e52
Preetam Jinka <pj@isomero.us>
2014-02-25 15:29:42 -0500

    Update traffic.c

--- traffic.c
+++ traffic.c
@@ -10,15 +10,20 @@ main() {
         getifaddrs(&ifap);
 
         printf("{\n");
-
-        for( ; ifap != NULL; ifap = ifap->ifa_next) {   
+        int first;
+        for(first = 1; ifap != NULL; ifap = ifap->ifa_next) {   
                 struct if_data* s = ifap->ifa_data;
-                if(s != NULL)
-                        printf("  \"%s\": {\n    \"bytesIn\": %d,\n    \"bytesOut\": %d\n  }\n", ifap->ifa_name, s->ifi_ibytes, s->ifi_obytes);
+                if(s != NULL) {
+                        if(!first) {
+                                printf(",\n");
+                        } else {
+                                first = 0;
+                        }
+                        printf("  \"%s\": {\n    \"bytesIn\": %d,\n    \"bytesOut\": %d\n  }",
+                                ifap->ifa_name, s->ifi_ibytes, s->ifi_obytes);
+                }
         }
         freeifaddrs(ifap);
 
-        printf("}\n");
-
+        printf("\n}\n");
 }
-
